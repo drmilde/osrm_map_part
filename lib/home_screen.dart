3004f382942f7f4ab407b3e846f6bc5c9d48b49f
drmilde <milde@hs-fulda.de>
2023-08-09 18:28:52 +0200

    korrektur der map daten bis ende E

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:osrm_map_part/screens/prototyp_dummies/osrm/osrm_list_screen.dart';
+import 'package:osrm_map_part/screens/suche/harvest_screen.dart';
+import 'package:osrm_map_part/screens/suche/map_correct_screen.dart';
 
 import 'screens/maps/polyline_screen.dart';
 import 'screens/suche/map_suche_screen.dart';
@@ -10,7 +12,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: OsrmListScreen()),
+      //body: Center(child: OsrmListScreen()),
+      //body: Center(child: HarvestScreen()),
+      body: Center(child: MapCorrectScreen()),
     );
   }
 }
--- lib/screens/suche/map_correct_screen.dart
+++ lib/screens/suche/map_correct_screen.dart
@@ -1,24 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:osrm_map_part/config/app_constants.dart';
 
 import '../../services/osrm/model/ortsnamen_mapping.dart';
-import '../prototyp_dummies/widgets/custom_popup_widget.dart';
-import '../prototyp_dummies/widgets/ortsnamen_textfield_widget.dart';
 
-class MapSucheScreen extends StatefulWidget {
+class MapCorrectScreen extends StatefulWidget {
   bool showBackArrow = false;
 
-  MapSucheScreen({this.showBackArrow = false, Key? key}) : super(key: key);
+  MapCorrectScreen({this.showBackArrow = false, Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-    return MapSucheScreenState();
+    return MapCorrectScreenState();
   }
 }
 
-class MapSucheScreenState extends State<MapSucheScreen> {
+class MapCorrectScreenState extends State<MapCorrectScreen> {
   AppConstants appConstants = AppConstants();
   double size = 160;
   late final _mapController;
@@ -26,6 +26,9 @@ class MapSucheScreenState extends State<MapSucheScreen> {
 
   late LatLng homePosition;
   late Marker homeMarker;
+
+  int ortsIndex = 0; // aktueller Ort
+  String ortsName = "Ortsname";
 
   @override
   void initState() {
@@ -57,27 +60,11 @@ class MapSucheScreenState extends State<MapSucheScreen> {
             children: [
               Row(
                 children: [
-                  widget.showBackArrow
-                      ? GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 8.0,
-                              right: 16,
-                            ),
-                            child: Icon(Icons.arrow_back_ios),
-                          ),
-                        )
-                      : Container(),
                   Expanded(
-                    child: Container(
-                      width: 200,
-                      child: OrtsnamenAutoCompleteWidget(
-                        onSelected: (value) {},
-                        mapController: _mapController,
-                        setMarker: _processLatLang,
+                    child: Center(
+                      child: Container(
+                        width: 200,
+                        child: Text(ortsName),
                       ),
                     ),
                   ),
@@ -85,20 +72,19 @@ class MapSucheScreenState extends State<MapSucheScreen> {
                   GestureDetector(
                     onTap: () async {
                       // TODO save home position in UserConfig
-                      print("save position");
-                      _savePosition(homePosition);
-
-                      CustomPopUp popup = CustomPopUp(
-                        content: _buildPopupForm(),
-                        title: "Ihr Abholort",
-                        button1Text: "Speichern",
-                      );
-                      var _ = await popup.showCustomDialog(context);
-
-                      Navigator.of(context).pop();
+                      ortsIndex = max(1, ortsIndex - 1);
+                      setState(() {
+                        ortsName = lookupOrtsnamen[ortsIndex].name;
+                        _processLatLang(lookupOrtsnamen[ortsIndex].latlng);
+                        // falls Karte, setzte den Punkt
+                        if (_mapController != null) {
+                          _mapController!
+                              .move(lookupOrtsnamen[ortsIndex].latlng, 13);
+                        }
+                      });
                     },
                     child: Container(
-                        width: 40,
+                        width: 100,
                         height: 40,
                         decoration: BoxDecoration(
                           color: Colors.white,
@@ -108,8 +94,37 @@ class MapSucheScreenState extends State<MapSucheScreen> {
                           ),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: Icon(Icons.save)),
+                        child: Icon(Icons.arrow_back_outlined)),
                   ),
+                  SizedBox(width: 16),
+                  GestureDetector(
+                    onTap: () async {
+                      // TODO save home position in UserConfig
+                      ortsIndex =
+                          min(lookupOrtsnamen.length - 1, ortsIndex + 1);
+                      setState(() {
+                        ortsName = lookupOrtsnamen[ortsIndex].name;
+                        _processLatLang(lookupOrtsnamen[ortsIndex].latlng);
+                        if (_mapController != null) {
+                          _mapController!
+                              .move(lookupOrtsnamen[ortsIndex].latlng, 13);
+                        }
+                      });
+                    },
+                    child: Container(
+                        width: 100,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(Icons.arrow_forward_outlined)),
+                  ),
+                  SizedBox(width: 64),
                 ],
               ),
               SizedBox(
@@ -141,21 +156,18 @@ class MapSucheScreenState extends State<MapSucheScreen> {
     );
   }
 
-  Widget _buildPopupForm() {
-    return Container();
+  void _handleTap(TapPosition tapPosition, LatLng latlng) {
+    _processClick(latlng);
   }
 
-  void _handleTap(TapPosition tapPosition, LatLng latlng) {
+  void _processClick(LatLng latlng) {
+    print(
+        "Mapping(name: \"${ortsName} (NEU ${DateTime.now()})\", latlng: LatLng(${latlng.latitude}, ${latlng.longitude})),");
     _processLatLang(latlng);
+
   }
 
   void _processLatLang(LatLng latlng) {
-    // TODO gib Ort in der Nähe aus
-    print(findNearest(latlng).name);
-    print ("Mapping(name: \"\", latlng: LatLng(${latlng.latitude}, ${latlng.longitude})),");
-
-    //_savePosition(latlng);
-
     homeMarker = Marker(
       width: size,
       height: size,
@@ -167,7 +179,6 @@ class MapSucheScreenState extends State<MapSucheScreen> {
     );
 
     homePosition = latlng;
-
     setState(() {});
   }
 
